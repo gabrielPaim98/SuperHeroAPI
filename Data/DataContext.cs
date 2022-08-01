322938d59bd5409e7fec7b2e9157bfba48a477f2
Gabriel Paim <g.paimm98@gmail.com>
2022-08-01 16:52:54 -0300

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApi;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
